b37fc7b86a479a55ccb1df92e2142ed3ee166e38
CTConsulting 7 <ctconsulting7@Irian-MacBook-Pro.local>
2018-03-14 16:30:09 -0600

    Little changes on prints

--- PyMerge.py
+++ PyMerge.py
@@ -6,30 +6,28 @@ import time
 asciimoji = ('ᕦ( ͡° ͜ʖ ͡°)ᕤ', '╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟ', '(╯°□°)╯︵ ┻━┻', 'ヽ(｀Д´)ﾉ')
 os.system('cls||clear')
 print('PyMerge Alpha0.1')
-print("WARNING! Both files MUST have same number of rows to create merged CSV correctly {0}".format(asciimoji[3]))
+print('WARNING! Both files MUST have same number of rows to create merged CSV correctly {0}'.format(asciimoji[3]))
+print('Header will be taked from FIRST CSV')
 # File 1
 while True:
     try:
-        csvLocation1 = str(input('Enter the name of first CSV to open like "file.csv": '))
+        csvLocation1 = str(input('Enter the name of FIRST CSV to open like "file.csv": '))
         csv1 = open(csvLocation1, newline = '', encoding='utf-8')
         read1 = list(csv.reader(csv1))
         break
     except IOError:
+        os.system('cls||clear')
         print('File not found, try again. {0}'.format(asciimoji[2]))
-    except ValueError:
-        print('Invalid number of rows, try again. {0}'.format(asciimoji[3]))
 # File 2
 while True:
     try:
-        csvLocation2 = str(input('Enter the name of second CSV to open like "file.csv": '))
+        csvLocation2 = str(input('Enter the name of SECOND CSV to open like "file.csv": '))
         csv2 = open(csvLocation2, newline = '', encoding='utf-8')
         csv2.__next__() #Header deleted
         read2 = list(csv.reader(csv2))
         break
     except IOError:
         print('File not found, try again. {0}'.format(asciimoji[2]))
-    except ValueError:
-        print('Invalid number of rows, try again. {0}'.format(asciimoji[3]))
 
 nameCSV = 'MergedCSV'
 newList = read1 #List without repeted elements
